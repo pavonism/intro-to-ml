a836023c1c1c6edcb8dbdbbb503f3de3968381b1
pavonism <spawlonka@outlook.com>
2024-11-02 18:13:16 +0100

    Add silence when merging audio segments

--- data_preprocessing/merging.py
+++ data_preprocessing/merging.py
@@ -7,14 +7,29 @@ from pydub import AudioSegment
 from tqdm import tqdm
 
 
-def merge_audios(segments: List[AudioSegment]) -> AudioSegment:
+def merge_audios(
+    segments: List[AudioSegment],
+    expected_duration_in_seconds: float = 3,
+) -> AudioSegment:
     merged_segment = AudioSegment.empty()
     for segment in segments:
         merged_segment += segment
+
+    if len(merged_segment) < expected_duration_in_seconds * 1000:
+        symmetric_silence_duration = (
+            expected_duration_in_seconds * 1000 - len(merged_segment)
+        ) // 2
+        silence = AudioSegment.silent(symmetric_silence_duration)
+        merged_segment = silence + merged_segment + silence
+
     return merged_segment
 
 
-def merge_audios_by_speakers(input_path: str, output_path: str):
+def merge_audios_by_speakers(
+    input_path: str,
+    output_path: str,
+    expected_duration_in_seconds: float = 3,
+):
     for dataset in ["train", "test", "validation"]:
         print(f"Merging audio files by speaker for {dataset} data...")
         for label in ["0", "1"]:
@@ -34,7 +49,10 @@ def merge_audios_by_speakers(input_path: str, output_path: str):
 
             for speaker_id, audio_files in tqdm(speaker_to_audio.items()):
                 audio_segments = [AudioSegment.from_wav(file) for file in audio_files]
-                merged_audio = merge_audios(audio_segments)
+                merged_audio = merge_audios(
+                    audio_segments,
+                    expected_duration_in_seconds=expected_duration_in_seconds,
+                )
                 merged_audio.export(
                     f"{output_directory}/{speaker_id}.wav",
                     format="wav",
